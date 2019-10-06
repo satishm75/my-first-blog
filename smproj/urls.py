796eceecb7db2fdebfa80a67a66f1d16626aed78
satishm75 <satish.mehta75@hotmail.com>
2019-10-06 04:54:44 -0400

    Modified ulg.py

--- smproj/urls.py
+++ smproj/urls.py
@@ -18,8 +18,17 @@ from django.urls import path
 from django.conf.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
+from blog import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+ path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+
